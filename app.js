46c1b5f70f61be72004064839556e8f33daf8561
Jovani Warguez <jwardash21@gmail.com>
2016-12-23 17:55:29 +0800

    Add Services Page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var nodeMailer = require('nodemailer');
 
 var index = require('./routes/index');
 var products = require('./routes/products');
+var services = require('./routes/services');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/products', products);
+app.use('/services', services);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
