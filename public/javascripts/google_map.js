7a3982a5eac3d939ee35be5bc3332492d8c87200
jovanidash21 <jwardash21@gmail.com>
2016-12-29 20:25:11 +0800

    Fix Google Maps Location

--- public/javascripts/google_map.js
+++ public/javascripts/google_map.js
@@ -30,7 +30,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Manila'];
+    var addresses = ['PUP College Of Engineering & Architecture Building'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
@@ -44,6 +44,6 @@ function init() {
 
         });
     }
-    
+
 }
 google.maps.event.addDomListener(window, 'load', init);
