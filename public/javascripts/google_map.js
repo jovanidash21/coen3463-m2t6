5403f4ec6a6428f86d49a367fd3fc7067cabe35e
jovanidash21 <jwardash21@gmail.com>
2016-12-29 19:02:07 +0800

    Update Google Maps Location

--- public/javascripts/google_map.js
+++ public/javascripts/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(14.5989661, 121.0053593);
     // 39.399872
     // -8.224454
     
