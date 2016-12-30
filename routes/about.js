cef590f216bf3c57b628e24c6e6e10219d5aa6f4
jovanidash21 <jwardash21@gmail.com>
2016-12-30 12:42:11 +0800

    Rename Folder

--- routes/about.js
+++ routes/about.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var servicesData = require('../datas/servicesData.json');
+var servicesData = require('../data/servicesData.json');
 
 router.get('/', function(req, res, next) {
     res.render('about', {
